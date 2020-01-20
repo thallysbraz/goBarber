24e7c29335196ed5d17edd285a8e2bd86358dd70
thallysbraz <thallysbraz3@gmail.com>
2020-01-20 14:11:33 -0300

    auth with JWT

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: "d31eea6de784321c97cd14231f7c617e",
+  expiresIn: "1d"
+};
--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,9 @@ import { Router } from "express";
 import User from "./app/models/User"; //Model de Usuário
 
 import UserController from "./app/controllers/UserController"; // UserController
-
+import SessionController from "./app/controllers/SessionController"; // SessionController
 const routes = new Router(); //instanciando rotas
 
 routes.post("/users", UserController.store);
-
+routes.post("/session", SessionController.store);
 export default routes;
