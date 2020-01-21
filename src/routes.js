5537adebebb14048383495b1427c36dc450e3788
thallysbraz <thallysbraz3@gmail.com>
2020-01-21 14:04:36 -0300

    config, teste e upload with multer

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
+import multer from "multer";
 
 import authMiddleware from "./app/middlewares/auth"; //middleware de autenticação
+import multerConfig from "./config/multer"; //config multer
 
 import User from "./app/models/User"; //Model de Usuário
 
@@ -8,6 +10,7 @@ import UserController from "./app/controllers/UserController"; // UserController
 import SessionController from "./app/controllers/SessionController"; // SessionController
 
 const routes = new Router(); //instanciando rotas
+const upload = multer(multerConfig);
 
 routes.post("/users", UserController.store); //rota para criar usuario
 routes.post("/session", SessionController.store); // rota pra autenticar usuario
@@ -15,4 +18,9 @@ routes.post("/session", SessionController.store); // rota pra autenticar usuario
 routes.use(authMiddleware); //Middleware global || valido para rotas abaixo
 
 routes.put("/users", UserController.update); //rota pra atualizar usuário
+
+routes.post("/files", upload.single("file"), (req, res) => {
+  return res.json({ ok: "true" });
+});
+
 export default routes;
