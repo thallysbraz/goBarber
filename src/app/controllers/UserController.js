7a1825b7483a0609a0e5d2a7d8e81709ecc3d753
thallysbraz <thallysbraz3@gmail.com>
2020-01-21 11:22:26 -0300

    update de usuário

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,6 +1,7 @@
 import User from "../models/User"; // Model de usuário
 
 class UserController {
+  //store para criar usuário
   async store(req, res) {
     //verificando se existe algum usuário com email registrado no Banco
     const userExists = await User.findOne({
@@ -17,10 +18,27 @@ class UserController {
     return res.json({ id, name, email, provider });
   }
 
+  //update para atualizar usuário
   async update(req, res) {
-    console.log(req.userId);
+    const { email, oldPassoword } = req.body;
 
-    return res.json({ msg: "ok, true" });
+    const user = await User.findByPk(req.userId); //buscando user
+    //verificando se usuário esta trocando email
+    if (email !== user.email) {
+      const userExists = await User.findOne({ where: { email } });
+
+      if (userExists) {
+        return res.status(400).json({ error: "User already exists" });
+      }
+    }
+    //verificando se a senha antiga está correta com a informada
+    if (oldPassoword && !(await user.checkPassword(oldPassoword))) {
+      return res.status(401).json({ error: "Password does not math. Invalid" });
+    }
+
+    const { id, name, provider } = await user.update(req.body);
+
+    return res.json({ id, name, email, provider });
   }
 }
 
