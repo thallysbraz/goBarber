c9396e38172670a2fb018b58ba0d31808ae7e650
thallysbraz <thallysbraz3@gmail.com>
2020-01-22 10:45:00 -0300

    fazendo validações no agendamento

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,15 +1,49 @@
 import Appointment from "../models/Appointment"; //Model de agendamentos
+import User from "../models/User";
 import * as Yup from "yup";
 
 class AppointmentController {
   async store(req, res) {
     const schema = Yup.object().shape({
-      //id e data obrigatorio
-      provider_id: yup.number().required(),
-      date: Yup.date().required()
+      provider_id: Yup.number() //id numero
+        .integer() //inteiro
+        .required(), //obrigatorio
+      date: Yup.date().required() //data obrigatoria
     });
 
-    return res.json({ ok: "true" });
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: "Validationfails" });
+    }
+
+    const { provider_id, date } = req.body;
+
+    if (provider_id === 7) {
+      /* 
+      Check if provider_id is a provider
+    */
+
+      console.log("number");
+    } else {
+      console.log("is number not");
+    }
+
+    const checkIsProvider = await User.findOne({
+      where: { id: provider_id, provider: true }
+    });
+
+    if (!checkIsProvider) {
+      return res
+        .status(401)
+        .json({ error: "You can only create appointments with providers" });
+    }
+
+    const appointment = await Appointment.create({
+      user_id: req.userId,
+      provider_id,
+      date
+    });
+
+    return res.json(appointment);
   }
 }
 
