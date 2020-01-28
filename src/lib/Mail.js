455462724c046d8b5af93dad36134043011128af
thallysbraz <thallysbraz3@gmail.com>
2020-01-28 13:32:37 -0300

    config templates de emails

--- src/lib/Mail.js
+++ src/lib/Mail.js
@@ -1,4 +1,7 @@
 import nodemailer from "nodemailer";
+import exphbs from "express-handlebars";
+import nodemailerhbs from "nodemailer-express-handlebars";
+import { resolve } from "path";
 
 import mailConfig from "../config/mail";
 
@@ -11,8 +14,28 @@ class Mail {
       secure,
       auth: auth.user ? auth : null
     });
+    this.configTemplates();
+  }
+  //config template
+  configTemplates() {
+    const viewPath = resolve(__dirname, "..", "app", "views", "emails");
+
+    this.transporter.use(
+      "compile",
+      nodemailerhbs({
+        viewEngine: exphbs.create({
+          layoutsDir: resolve(viewPath, "layouts"),
+          partialsDir: resolve(viewPath, "partials"),
+          defaultLayout: "default",
+          extname: ".hbs"
+        }),
+        viewPath,
+        extName: ".hbs"
+      })
+    );
   }
 
+  //config de mensagem
   sendMail(message) {
     return this.transporter.sendMail({
       ...mailConfig.default,
