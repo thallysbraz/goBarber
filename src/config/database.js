07024d51b58394be3a23d491c5b62eb692d65f3e
thallysbraz <thallysbraz3@gmail.com>
2020-01-29 11:43:09 -0300

    connection with dataBases

--- src/config/database.js
+++ src/config/database.js
@@ -11,3 +11,19 @@ module.exports = {
     underscoredAll: true
   }
 };
+/*  DataBase with docker
+
+module.exports = {
+  dialect: "postgres",
+  host: "localhost",
+  username: "postgres",
+  password: "First2019",
+  database: "goBarber",
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  }
+};
+
+*/
